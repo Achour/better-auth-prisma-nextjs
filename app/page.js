22e2a30319bd359efdc2801b521b4f2ccb9ece6d
Meguenni Achour <achourphp@gmail.com>
2025-03-22 03:03:04 +0100

    Fix import case for LandingPage component in page.js

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 import { headers } from "next/headers";
 import { auth } from "@/lib/auth";
-import LandingPage from "./landingpage";
+import LandingPage from "./LandingPage";
 export default async function Home() {
   const session = await auth.api.getSession({
     headers: await headers(), // you need to pass the headers object.
